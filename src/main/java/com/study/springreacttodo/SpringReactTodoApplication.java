a5f6fa51dca8908eb3c6f8b17f4b22dc4c4232e7
gynhyunkim <gyeonghyun.kimm@gmail.com>
2023-10-31 22:38:44 +0900

    [init] init commit

---
+++ src/main/java/com/study/springreacttodo/SpringReactTodoApplication.java
@@ -0,0 +1,13 @@
+package com.study.springreacttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springreacttodo/SpringReactTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springreacttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
